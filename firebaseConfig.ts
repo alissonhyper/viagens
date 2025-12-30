f1b81ff15e17131806f5cc2c236b7e76206ea929
alissonhyper <alissonkas2@gmail.com>
2025-12-30 09:52:28 -0300

    feat(firebase): Adiciona arquivos de configuração do Firebase e atualiza o lock

---
+++ firebaseConfig.ts
@@ -0,0 +1,20 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
+};
+
+// Inicializar o Firebase
+const app = initializeApp(firebaseConfig);
+
+// Inicializar o Firestore (Banco de Dados)
+export const db = getFirestore(app);
+
+// Exportar o app para outros serviços (ex: Auth)
+export default app;
